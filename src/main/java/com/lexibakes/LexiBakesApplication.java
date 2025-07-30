d47619cdc4b84de4c89afcbb8ad9932b830ccfc7
Tim Heinig <timheinig@Tims-MBP.lan>
2025-07-30 19:58:16 -0400

    Initial Lexi Bakes site

---
+++ src/main/java/com/lexibakes/LexiBakesApplication.java
@@ -0,0 +1,13 @@
+package com.lexibakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexiBakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexiBakesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexibakes/LexiBakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexibakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexiBakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
